e95381a66800ffa5270988967459b53a2ebab1ec
Nouhoum TRAORE <nouhoum.traore@gmail.com>
2013-04-22 23:37:23 +0200

    Refactoring

--- src/euler/problem1.clj
+++ src/euler/problem1.clj
@@ -1,8 +1,7 @@
 (ns euler.problem1)
 
-(defn mult3-and-5 [upper]
-  (for [x (range upper) :when (or (= 0 (rem x 3))
-                                        (= 0 (rem x 5)))] x))                             
-
 (defn sum-multiple-of-three-and-five [upper]
-  (reduce + 0 (mult3-and-5 upper)))
+  (let [mult3-and-5 (fn [upper]
+                      (for [x (range upper) :when (or (= 0 (rem x 3))
+                                        (= 0 (rem x 5)))] x))]
+  (reduce + 0 (mult3-and-5 upper))))
