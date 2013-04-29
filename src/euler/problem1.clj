063594b5b40bea8e52bdf4dde2d2eac4a06ef5fe
Nouhoum TRAORE <nouhoum.traore@gmail.com>
2013-04-30 00:24:10 +0200

    Add a new implementation of problem one with letfn syntax

--- src/euler/problem1.clj
+++ src/euler/problem1.clj
@@ -1,7 +1,18 @@
 (ns euler.problem1)
 
 (defn sum-multiple-of-three-and-five [upper]
-  (let [mult3-and-5 (fn [upper]
+  (let [mult3-or-5 (fn [upper]
                       (for [x (range upper) :when (or (= 0 (rem x 3))
                                         (= 0 (rem x 5)))] x))]
-  (reduce + 0 (mult3-and-5 upper))))
+  (reduce + 0 (mult3-or-5 upper))))
+
+(defn sum-multiple-of-three-and-five2 [upper]
+  (letfn [(multiple-of-three? [x]
+            (= 0 (rem x 3)))
+          (multiple-of-five? [y]
+            (= 0 (rem y 5)))]
+    (reduce + 0 (for [x (range upper) :when (or (multiple-of-three? x)
+                                                (multiple-of-five? x))
+                      ] x))))
+                          
+                            
